aef27e00d52a1772377eb5a37272dc4b1c3b29ae
elliedotjpg <ellieacharon@gmail.com>
2025-02-28 23:57:45 +0800

    🚧 Made attempt for adding videos to gallery

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ class GalleryPreview extends HTMLElement {
     connectedCallback() {
       const template = document.querySelector('#preview-template');
       const templateContent = template.content.cloneNode(true);
-  
       // Populate content with attributes from the HTML
       templateContent.querySelector('.preview-image').src = this.getAttribute('src');
       templateContent.querySelector('.preview-image').alt = this.getAttribute('title');
@@ -14,8 +13,25 @@ class GalleryPreview extends HTMLElement {
     }
   }
 
+  class VideoGalleryPreview extends HTMLElement {
+    connectedCallback() {
+      const videoTemplate = document.querySelector('#video-preview-template');
+      const videoTemplateContent = videoTemplate.content.cloneNode(true);
+      // Populate content with attributes from the HTML
+
+      videoTemplateContent.querySelector('.preview-video').src = this.getAttribute('video-src');
+      videoTemplateContent.querySelector('.preview-video').alt = this.getAttribute('video-title');
+      videoTemplateContent.querySelector('.vid-prev-software').textContent = this.getAttribute('video-software');
+      videoTemplateContent.querySelector('.vid-prev-title').textContent = this.getAttribute('video-title');
+      videoTemplateContent.querySelector('.vid-prev-description').textContent = this.getAttribute('video-desc');
+  
+      this.appendChild(videoTemplateContent);
+    }
+  }
+
   // Register the custom element
   customElements.define('gallery-preview', GalleryPreview);
+  customElements.define('video-gallery-preview', VideoGalleryPreview);
 
 
   
